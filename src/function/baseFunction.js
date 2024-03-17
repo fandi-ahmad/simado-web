fc71115a019f94bf5c7c728920b3a1ddd670885a
fandi-ahmad <fandi4160@gmail.com>
2024-03-17 15:13:20 +0800

    add funcion for date formated

--- src/function/baseFunction.js
+++ src/function/baseFunction.js
@@ -43,3 +43,18 @@ export const limitText = (text, limit = 30) => {
     return text.substring(0, limit) + "...";
   }
 }
+
+export const formatDateFromISO = (dateString) => {
+  const tanggalISO = dateString
+  const tanggalObjek = new Date(tanggalISO);
+  const namaBulan = [
+    "Januari", "Februari", "Maret", "April", "Mei", "Juni", 
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+  ];
+
+  const tanggal = tanggalObjek.getUTCDate().toString().padStart(2, '0');
+  const bulan = namaBulan[tanggalObjek.getUTCMonth()];
+  const tahun = tanggalObjek.getUTCFullYear();
+  const hasil = `${tanggal} ${bulan} ${tahun}`;
+  return hasil
+}
