d6cd8943e09ced8de685ad30b3b7447ae0524f28
fandi-ahmad <fandi4160@gmail.com>
2024-03-26 16:27:21 +0800

    fetch data for dashboard

--- src/components/BaseCard.jsx
+++ src/components/BaseCard.jsx
@@ -3,20 +3,22 @@ import React from 'react'
 export const CardData = (props) => {
   return (
     <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
-      <div className="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+      <div className="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-xl bg-clip-border">
         <div className="flex-auto p-4">
           <div className="flex flex-row -mx-3">
-            <div className="flex-none w-2/3 max-w-full px-3">
+            <div className={`${props.icon ? 'w-2/3' : ''} flex-none max-w-full px-3`}>
               <div>
                 <p className="mb-0 text-sm font-semibold leading-normal">{props.text}</p>
                 <h5 className="mb-0 font-bold">{props.value}</h5>
               </div>
             </div>
+            {props.icon ?
             <div className="px-3 text-right basis-1/3">
               <div className="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
-                <i className={`fa-solid ${props.icon || 'fa-file'} text-lg relative top-2.5 text-white`}></i>
+                <i className={`fa-solid ${props.icon} text-lg relative top-2.5 text-white`}></i>
               </div>
-            </div>
+            </div> : null
+            }
           </div>
         </div>
       </div>
